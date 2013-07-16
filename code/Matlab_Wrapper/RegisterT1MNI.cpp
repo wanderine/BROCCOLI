83678891cf6f412c258dadfc9008c62209c8fa9e
Anders Eklund <andek034@gmail.com>
2013-07-16 00:02:54 -0400

    T1 MNI registration and skullstrip in Matlab

---
+++ code/Matlab_Wrapper/RegisterT1MNI.cpp
@@ -0,0 +1,258 @@
+/*
+ * BROCCOLI: An open source multi-platform software for parallel analysis of fMRI data on many core CPUs and GPUS
+ * Copyright (C) <2013>  Anders Eklund, andek034@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "mex.h"
+#include "help_functions.cpp"
+#include "broccoli_lib.h"
+
+void cleanUp()
+{
+    //cudaDeviceReset();
+}
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    //-----------------------
+    // Input pointers
+    
+    double		    *h_fMRI_Volumes_double, *h_Quadrature_Filter_1_Real_double, *h_Quadrature_Filter_2_Real_double, *h_Quadrature_Filter_3_Real_double, *h_Quadrature_Filter_1_Imag_double, *h_Quadrature_Filter_2_Imag_double, *h_Quadrature_Filter_3_Imag_double;
+    float           *h_fMRI_Volumes, *h_Quadrature_Filter_1_Real, *h_Quadrature_Filter_2_Real, *h_Quadrature_Filter_3_Real, *h_Quadrature_Filter_1_Imag, *h_Quadrature_Filter_2_Imag, *h_Quadrature_Filter_3_Imag;
+    int             MOTION_CORRECTION_FILTER_SIZE, NUMBER_OF_ITERATIONS_FOR_MOTION_CORRECTION;
+    
+    //-----------------------
+    // Output pointers        
+    
+    double     		*h_Motion_Corrected_fMRI_Volumes_double, *h_Motion_Parameters_double;
+    double          *h_Quadrature_Filter_Response_1_Real_double, *h_Quadrature_Filter_Response_1_Imag_double;
+    double          *h_Quadrature_Filter_Response_2_Real_double, *h_Quadrature_Filter_Response_2_Imag_double;
+    double          *h_Quadrature_Filter_Response_3_Real_double, *h_Quadrature_Filter_Response_3_Imag_double;
+    double          *h_Phase_Differences_double, *h_Phase_Certainties_double, *h_Phase_Gradients_double;
+    float           *h_Quadrature_Filter_Response_1_Real, *h_Quadrature_Filter_Response_1_Imag;
+    float           *h_Quadrature_Filter_Response_2_Real, *h_Quadrature_Filter_Response_2_Imag;
+    float           *h_Quadrature_Filter_Response_3_Real, *h_Quadrature_Filter_Response_3_Imag;  
+    float           *h_Phase_Differences, *h_Phase_Certainties, *h_Phase_Gradients;
+    float           *h_Motion_Corrected_fMRI_Volumes, *h_Motion_Parameters;
+    
+    //---------------------
+    
+    /* Check the number of input and output arguments. */
+    if(nrhs<5)
+    {
+        mexErrMsgTxt("Too few input arguments.");
+    }
+    if(nrhs>5)
+    {
+        mexErrMsgTxt("Too many input arguments.");
+    }
+    if(nlhs<8)
+    {
+        mexErrMsgTxt("Too few output arguments.");
+    }
+    if(nlhs>8)
+    {
+        mexErrMsgTxt("Too many output arguments.");
+    }
+    
+    /* Input arguments */
+    
+    // The data
+    h_fMRI_Volumes_double =  (double*)mxGetData(prhs[0]);
+    h_Quadrature_Filter_1_Real_double =  (double*)mxGetPr(prhs[1]);
+    h_Quadrature_Filter_1_Imag_double =  (double*)mxGetPi(prhs[1]);
+    h_Quadrature_Filter_2_Real_double =  (double*)mxGetPr(prhs[2]);
+    h_Quadrature_Filter_2_Imag_double =  (double*)mxGetPi(prhs[2]);
+    h_Quadrature_Filter_3_Real_double =  (double*)mxGetPr(prhs[3]);
+    h_Quadrature_Filter_3_Imag_double =  (double*)mxGetPi(prhs[3]);
+    NUMBER_OF_ITERATIONS_FOR_MOTION_CORRECTION  = (int)mxGetScalar(prhs[4]);
+    
+    int NUMBER_OF_DIMENSIONS = mxGetNumberOfDimensions(prhs[0]);
+    const int *ARRAY_DIMENSIONS_DATA = mxGetDimensions(prhs[0]);
+    const int *ARRAY_DIMENSIONS_FILTER = mxGetDimensions(prhs[1]);
+    
+    int DATA_H, DATA_W, DATA_D, DATA_T, NUMBER_OF_MOTION_CORRECTION_PARAMETERS;
+    NUMBER_OF_MOTION_CORRECTION_PARAMETERS = 12;
+    
+    DATA_H = ARRAY_DIMENSIONS_DATA[0];
+    DATA_W = ARRAY_DIMENSIONS_DATA[1];
+    DATA_D = ARRAY_DIMENSIONS_DATA[2];
+    DATA_T = ARRAY_DIMENSIONS_DATA[3];
+                
+    MOTION_CORRECTION_FILTER_SIZE = ARRAY_DIMENSIONS_FILTER[0];
+            
+    int DATA_SIZE = DATA_W * DATA_H * DATA_D * DATA_T * sizeof(float);
+    int MOTION_PARAMETERS_SIZE = NUMBER_OF_MOTION_CORRECTION_PARAMETERS * DATA_T * sizeof(float);
+    int FILTER_SIZE = MOTION_CORRECTION_FILTER_SIZE * MOTION_CORRECTION_FILTER_SIZE * MOTION_CORRECTION_FILTER_SIZE * sizeof(float);
+    int VOLUME_SIZE = DATA_W * DATA_H * DATA_D * sizeof(float);
+    
+    mexPrintf("Data size : %i x %i x %i x %i \n",  DATA_W, DATA_H, DATA_D, DATA_T);
+    mexPrintf("Filter size : %i x %i x %i \n",  MOTION_CORRECTION_FILTER_SIZE,MOTION_CORRECTION_FILTER_SIZE,MOTION_CORRECTION_FILTER_SIZE);
+    mexPrintf("Number of iterations : %i \n",  NUMBER_OF_ITERATIONS_FOR_MOTION_CORRECTION);
+    
+    //-------------------------------------------------
+    // Output to Matlab
+    
+    // Create pointer for volumes to Matlab        
+    NUMBER_OF_DIMENSIONS = 4;
+    int ARRAY_DIMENSIONS_OUT_MOTION_CORRECTED_FMRI_VOLUMES[4];
+    ARRAY_DIMENSIONS_OUT_MOTION_CORRECTED_FMRI_VOLUMES[0] = DATA_H;
+    ARRAY_DIMENSIONS_OUT_MOTION_CORRECTED_FMRI_VOLUMES[1] = DATA_W;
+    ARRAY_DIMENSIONS_OUT_MOTION_CORRECTED_FMRI_VOLUMES[2] = DATA_D;
+    ARRAY_DIMENSIONS_OUT_MOTION_CORRECTED_FMRI_VOLUMES[3] = DATA_T;
+    
+    plhs[0] = mxCreateNumericArray(NUMBER_OF_DIMENSIONS,ARRAY_DIMENSIONS_OUT_MOTION_CORRECTED_FMRI_VOLUMES,mxDOUBLE_CLASS, mxREAL);
+    h_Motion_Corrected_fMRI_Volumes_double = mxGetPr(plhs[0]);          
+    
+    NUMBER_OF_DIMENSIONS = 2;
+    int ARRAY_DIMENSIONS_OUT_MOTION_PARAMETERS[2];
+    ARRAY_DIMENSIONS_OUT_MOTION_PARAMETERS[0] = DATA_T;
+    ARRAY_DIMENSIONS_OUT_MOTION_PARAMETERS[1] = NUMBER_OF_MOTION_CORRECTION_PARAMETERS;    
+    
+    plhs[1] = mxCreateNumericArray(NUMBER_OF_DIMENSIONS,ARRAY_DIMENSIONS_OUT_MOTION_PARAMETERS,mxDOUBLE_CLASS, mxREAL);
+    h_Motion_Parameters_double = mxGetPr(plhs[1]);          
+    
+    NUMBER_OF_DIMENSIONS = 3;
+    int ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE[3];
+    ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE[0] = DATA_H;
+    ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE[1] = DATA_W;    
+    ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE[2] = DATA_D;  
+    
+    plhs[2] = mxCreateNumericArray(NUMBER_OF_DIMENSIONS,ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE,mxDOUBLE_CLASS, mxCOMPLEX);
+    h_Quadrature_Filter_Response_1_Real_double = mxGetPr(plhs[2]);          
+    h_Quadrature_Filter_Response_1_Imag_double = mxGetPi(plhs[2]);          
+    
+    plhs[3] = mxCreateNumericArray(NUMBER_OF_DIMENSIONS,ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE,mxDOUBLE_CLASS, mxCOMPLEX);
+    h_Quadrature_Filter_Response_2_Real_double = mxGetPr(plhs[3]);          
+    h_Quadrature_Filter_Response_2_Imag_double = mxGetPi(plhs[3]);          
+        
+    plhs[4] = mxCreateNumericArray(NUMBER_OF_DIMENSIONS,ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE,mxDOUBLE_CLASS, mxCOMPLEX);
+    h_Quadrature_Filter_Response_3_Real_double = mxGetPr(plhs[4]);          
+    h_Quadrature_Filter_Response_3_Imag_double = mxGetPi(plhs[4]);          
+                
+    plhs[5] = mxCreateNumericArray(NUMBER_OF_DIMENSIONS,ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE,mxDOUBLE_CLASS, mxREAL);
+    h_Phase_Differences_double = mxGetPr(plhs[5]);          
+    
+    plhs[6] = mxCreateNumericArray(NUMBER_OF_DIMENSIONS,ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE,mxDOUBLE_CLASS, mxREAL);
+    h_Phase_Certainties_double = mxGetPr(plhs[6]);          
+    
+    plhs[7] = mxCreateNumericArray(NUMBER_OF_DIMENSIONS,ARRAY_DIMENSIONS_OUT_FILTER_RESPONSE,mxDOUBLE_CLASS, mxREAL);
+    h_Phase_Gradients_double = mxGetPr(plhs[7]);          
+    
+    // ------------------------------------------------
+    
+    // Allocate memory on the host
+    h_fMRI_Volumes                         = (float *)mxMalloc(DATA_SIZE);
+    h_Quadrature_Filter_1_Real             = (float *)mxMalloc(FILTER_SIZE);
+    h_Quadrature_Filter_1_Imag             = (float *)mxMalloc(FILTER_SIZE);
+    h_Quadrature_Filter_2_Real             = (float *)mxMalloc(FILTER_SIZE);
+    h_Quadrature_Filter_2_Imag             = (float *)mxMalloc(FILTER_SIZE);    
+    h_Quadrature_Filter_3_Real             = (float *)mxMalloc(FILTER_SIZE);
+    h_Quadrature_Filter_3_Imag             = (float *)mxMalloc(FILTER_SIZE);    
+    h_Quadrature_Filter_Response_1_Real    = (float *)mxMalloc(VOLUME_SIZE);
+    h_Quadrature_Filter_Response_1_Imag    = (float *)mxMalloc(VOLUME_SIZE);
+    h_Quadrature_Filter_Response_2_Real    = (float *)mxMalloc(VOLUME_SIZE);
+    h_Quadrature_Filter_Response_2_Imag    = (float *)mxMalloc(VOLUME_SIZE);
+    h_Quadrature_Filter_Response_3_Real    = (float *)mxMalloc(VOLUME_SIZE);
+    h_Quadrature_Filter_Response_3_Imag    = (float *)mxMalloc(VOLUME_SIZE);    
+    h_Phase_Differences                    = (float *)mxMalloc(VOLUME_SIZE);    
+    h_Phase_Certainties                    = (float *)mxMalloc(VOLUME_SIZE);  
+    h_Phase_Gradients                      = (float *)mxMalloc(VOLUME_SIZE);  
+    h_Motion_Corrected_fMRI_Volumes        = (float *)mxMalloc(DATA_SIZE);
+    h_Motion_Parameters                    = (float *)mxMalloc(MOTION_PARAMETERS_SIZE);
+    
+    // Reorder and cast data
+    pack_double2float_volumes(h_fMRI_Volumes, h_fMRI_Volumes_double, DATA_W, DATA_H, DATA_D, DATA_T);
+    pack_double2float_volume(h_Quadrature_Filter_1_Real, h_Quadrature_Filter_1_Real_double, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE);
+    pack_double2float_volume(h_Quadrature_Filter_1_Imag, h_Quadrature_Filter_1_Imag_double, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE);
+    pack_double2float_volume(h_Quadrature_Filter_2_Real, h_Quadrature_Filter_2_Real_double, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE);
+    pack_double2float_volume(h_Quadrature_Filter_2_Imag, h_Quadrature_Filter_2_Imag_double, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE);
+    pack_double2float_volume(h_Quadrature_Filter_3_Real, h_Quadrature_Filter_3_Real_double, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE);
+    pack_double2float_volume(h_Quadrature_Filter_3_Imag, h_Quadrature_Filter_3_Imag_double, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE, MOTION_CORRECTION_FILTER_SIZE);
+
+    //------------------------
+    
+    BROCCOLI_LIB BROCCOLI;
+    
+    BROCCOLI.SetWidth(DATA_W);
+    BROCCOLI.SetHeight(DATA_H);
+    BROCCOLI.SetDepth(DATA_D);
+    BROCCOLI.SetTimepoints(DATA_T);   
+    BROCCOLI.SetGlobalAndLocalWorkSizes();
+    BROCCOLI.SetInputfMRIVolumes(h_fMRI_Volumes);
+    BROCCOLI.SetMotionCorrectionFilterSize(MOTION_CORRECTION_FILTER_SIZE);
+    BROCCOLI.SetMotionCorrectionFilters(h_Quadrature_Filter_1_Real, h_Quadrature_Filter_1_Imag, h_Quadrature_Filter_2_Real, h_Quadrature_Filter_2_Imag, h_Quadrature_Filter_3_Real, h_Quadrature_Filter_3_Imag);
+    BROCCOLI.SetNumberOfIterationsForMotionCorrection(NUMBER_OF_ITERATIONS_FOR_MOTION_CORRECTION);
+    BROCCOLI.SetOutputData(h_Motion_Corrected_fMRI_Volumes);
+    BROCCOLI.SetOutputMotionParameters(h_Motion_Parameters);
+    BROCCOLI.SetOutputQuadratureFilterResponses(h_Quadrature_Filter_Response_1_Real, h_Quadrature_Filter_Response_1_Imag, h_Quadrature_Filter_Response_2_Real, h_Quadrature_Filter_Response_2_Imag, h_Quadrature_Filter_Response_3_Real, h_Quadrature_Filter_Response_3_Imag);
+    BROCCOLI.SetOutputPhaseDifferences(h_Phase_Differences);
+    BROCCOLI.SetOutputPhaseCertainties(h_Phase_Certainties);
+    BROCCOLI.SetOutputPhaseGradients(h_Phase_Gradients);
+    
+    mexPrintf("Device info \n \n %s \n", BROCCOLI.GetOpenCLDeviceInfoChar());
+    mexPrintf("Build info \n \n %s \n", BROCCOLI.GetOpenCLBuildInfoChar());
+            
+    BROCCOLI.PerformMotionCorrectionTest();
+    
+    int error = BROCCOLI.GetOpenCLError();
+    mexPrintf("Error is %d \n",error);
+    
+    int createKernelError = BROCCOLI.GetOpenCLCreateKernelError();
+    mexPrintf("Create kernel error is %d \n",createKernelError);
+    
+    double convolution_time = BROCCOLI.GetProcessingTimeConvolution();
+    mexPrintf("Convolution time is %f ms \n",convolution_time/1000000.0);
+    
+    
+    unpack_float2double_volumes(h_Motion_Corrected_fMRI_Volumes_double, h_Motion_Corrected_fMRI_Volumes, DATA_W, DATA_H, DATA_D, DATA_T);
+    unpack_float2double(h_Motion_Parameters_double, h_Motion_Parameters, NUMBER_OF_MOTION_CORRECTION_PARAMETERS * DATA_T);
+    unpack_float2double_volume(h_Quadrature_Filter_Response_1_Real_double, h_Quadrature_Filter_Response_1_Real, DATA_W, DATA_H, DATA_D);
+    unpack_float2double_volume(h_Quadrature_Filter_Response_1_Imag_double, h_Quadrature_Filter_Response_1_Imag, DATA_W, DATA_H, DATA_D);
+    unpack_float2double_volume(h_Quadrature_Filter_Response_2_Real_double, h_Quadrature_Filter_Response_2_Real, DATA_W, DATA_H, DATA_D);
+    unpack_float2double_volume(h_Quadrature_Filter_Response_2_Imag_double, h_Quadrature_Filter_Response_2_Imag, DATA_W, DATA_H, DATA_D);
+    unpack_float2double_volume(h_Quadrature_Filter_Response_3_Real_double, h_Quadrature_Filter_Response_3_Real, DATA_W, DATA_H, DATA_D);
+    unpack_float2double_volume(h_Quadrature_Filter_Response_3_Imag_double, h_Quadrature_Filter_Response_3_Imag, DATA_W, DATA_H, DATA_D);
+    unpack_float2double_volume(h_Phase_Differences_double, h_Phase_Differences, DATA_W, DATA_H, DATA_D);
+    unpack_float2double_volume(h_Phase_Certainties_double, h_Phase_Certainties, DATA_W, DATA_H, DATA_D);
+    unpack_float2double_volume(h_Phase_Gradients_double, h_Phase_Gradients, DATA_W, DATA_H, DATA_D);    
+    
+    // Free all the allocated memory on the host
+    mxFree(h_fMRI_Volumes);
+    mxFree(h_Quadrature_Filter_1_Real);
+    mxFree(h_Quadrature_Filter_1_Imag);
+    mxFree(h_Quadrature_Filter_2_Real);
+    mxFree(h_Quadrature_Filter_2_Imag);
+    mxFree(h_Quadrature_Filter_3_Real);
+    mxFree(h_Quadrature_Filter_3_Imag);
+    mxFree(h_Quadrature_Filter_Response_1_Real);
+    mxFree(h_Quadrature_Filter_Response_1_Imag);
+    mxFree(h_Quadrature_Filter_Response_2_Real);
+    mxFree(h_Quadrature_Filter_Response_2_Imag);
+    mxFree(h_Quadrature_Filter_Response_3_Real);
+    mxFree(h_Quadrature_Filter_Response_3_Imag);
+    mxFree(h_Phase_Differences);
+    mxFree(h_Phase_Certainties);
+    mxFree(h_Phase_Gradients);
+    mxFree(h_Motion_Corrected_fMRI_Volumes); 
+    mxFree(h_Motion_Parameters);
+    
+    //mexAtExit(cleanUp);
+    
+    return;
+}
+
+
---
+++ code/Matlab_Wrapper/test_T1_MNI_registration.m
@@ -0,0 +1,214 @@
+%  	 BROCCOLI: An open source multi-platform software for parallel analysis of fMRI data on many core CPUs and GPUS
+%    Copyright (C) <2013>  Anders Eklund, andek034@gmail.com
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%-----------------------------------------------------------------------------
+
+%---------------------------------------------------------------------------------------------------------------------
+% README
+% If you run this code in Windows, your graphics driver might stop working
+% for large volumes / large filter sizes. This is not a bug in my code but is due to the
+% fact that the Nvidia driver thinks that something is wrong if the GPU
+% takes more than 2 seconds to complete a task. This link solved my problem
+% https://forums.geforce.com/default/topic/503962/tdr-fix-here-for-nvidia-driver-crashing-randomly-in-firefox/
+%---------------------------------------------------------------------------------------------------------------------
+
+clear all
+clc
+close all
+
+addpath('D:\nifti_matlab')
+
+%mex RegisterT1MNI.cpp -lOpenCL -lBROCCOLI_LIB -IC:/Program' Files'/NVIDIA' GPU Computing Toolkit'/CUDA/v5.0/include -IC:/Program' Files'/NVIDIA' GPU Computing Toolkit'/CUDA/v5.0/include/CL -LC:/Program' Files'/NVIDIA' GPU Computing Toolkit'/CUDA/v5.0/lib/x64 -LC:/users/wande/Documents/Visual' Studio 2010'/Projects/BROCCOLI_LIB/x64/Release/ -IC:/users/wande/Documents/Visual' Studio 2010'/Projects/BROCCOLI_LIB/BROCCOLI_LIB -IC:\Users\wande\Documents\Visual' Studio 2010'\Projects\BROCCOLI_LIB\nifticlib-2.0.0\niftilib  -IC:\Users\wande\Documents\Visual' Studio 2010'\Projects\BROCCOLI_LIB\nifticlib-2.0.0\znzlib
+
+%mex -g RegisterT1MNI.cpp -lOpenCL -lBROCCOLI_LIB -IC:/Program' Files'/NVIDIA' GPU Computing Toolkit'/CUDA/v5.0/include -IC:/Program' Files'/NVIDIA' GPU Computing Toolkit'/CUDA/v5.0/include/CL -LC:/Program' Files'/NVIDIA' GPU Computing Toolkit'/CUDA/v5.0/lib/x64 -LC:/users/wande/Documents/Visual' Studio 2010'/Projects/BROCCOLI_LIB/x64/Debug/ -IC:/users/wande/Documents/Visual' Studio 2010'/Projects/BROCCOLI_LIB/BROCCOLI_LIB -IC:\Users\wande\Documents\Visual' Studio 2010'\Projects\BROCCOLI_LIB\nifticlib-2.0.0\niftilib  -IC:\Users\wande\Documents\Visual' Studio 2010'\Projects\BROCCOLI_LIB\nifticlib-2.0.0\znzlib
+
+T1_nii = load_nii('../../test_data/T1_AE.nii');
+T1 = double(T1_nii.img);
+T1 = T1(1:182,1:218,105:end);
+MNI_nii = load_nii('../../test_data/MNI152_T1_1mm.nii');
+MNI = double(MNI_nii.img);
+MNI = MNI(:,:,1:end-30);
+[sy sx sz] = size(T1)
+[sy sx sz] = size(MNI)
+
+mask_nii = load_nii('../../test_data/MNI152_T1_1mm_brain_mask.nii');
+mask = double(mask_nii.img);
+mask = mask(:,:,1:end-30);
+
+load filters.mat
+
+T1_ = T1(1:4:end,1:4:end,1:4:end);
+[sy sx sz] = size(T1_)
+MNI_ = MNI(1:4:end,1:4:end,1:4:end);
+
+number_of_iterations_for_motion_correction = 40;
+[registered_T1_cpu, registration_parameters_cpu1] = perform_T1_MNI_registration_CPU(T1_,MNI_,f1,f2,f3,number_of_iterations_for_motion_correction);
+
+%tic
+%[registered_T1_opencl, registration_parameters_opencl, quadrature_filter_response_1_opencl, quadrature_filter_response_2_opencl, quadrature_filter_response_3_opencl, phase_differences_x_opencl, phase_certainties_x_opencl, phase_gradients_x_opencl] = RegisterT1MNI(T1,MNI,f1,f2,f3,number_of_iterations_for_motion_correction);
+%toc
+
+slice = 32;
+
+figure
+imagesc([ MNI_(:,:,slice)/norm(MNI_(:)) T1_(:,:,slice)/norm(T1_(:)) ] ); colormap gray
+figure
+imagesc([ MNI_(:,:,slice)/norm(MNI_(:)) registered_T1_cpu(:,:,slice)/norm(registered_T1_cpu(:)) ] ); colormap gray
+
+T1_ = T1(1:2:end,1:2:end,1:2:end);
+MNI_ = MNI(1:2:end,1:2:end,1:2:end);
+[sy sx sz] = size(T1_)
+p = registration_parameters_cpu1;
+p(1) = p(1)*2;
+p(2) = p(2)*2;
+p(3) = p(3)*2;
+[x, y, z] = meshgrid(-(sx-1)/2:(sx-1)/2,-(sy-1)/2:(sy-1)/2, -(sz-1)/2:(sz-1)/2);
+x_motion_vectors = zeros(sy,sx,sz);
+y_motion_vectors = zeros(sy,sx,sz);
+z_motion_vectors = zeros(sy,sx,sz);
+x_motion_vectors(:) = p(1) + [x(:) y(:) z(:)]*p(4:6);
+y_motion_vectors(:) = p(2) + [x(:) y(:) z(:)]*p(7:9);
+z_motion_vectors(:) = p(3) + [x(:) y(:) z(:)]*p(10:12);
+
+T1__ = interp3(x,y,z,T1_,x+x_motion_vectors,y+y_motion_vectors,z+z_motion_vectors,'linear');    % Generates NaN's
+T1__(isnan(T1__)) = 0;
+
+number_of_iterations_for_motion_correction = 10;
+[registered_T1_cpu, registration_parameters_cpu2] = perform_T1_MNI_registration_CPU(T1__,MNI_,f1,f2,f3,number_of_iterations_for_motion_correction);
+
+slice = 64;
+
+figure
+imagesc([ MNI_(:,:,slice)/norm(MNI_(:)) T1__(:,:,slice)/norm(T1__(:)) ] ); colormap gray
+figure
+imagesc([ MNI_(:,:,slice)/norm(MNI_(:)) registered_T1_cpu(:,:,slice)/norm(registered_T1_cpu(:)) ] ); colormap gray
+
+
+[sy sx sz] = size(T1)
+p(1:3) = registration_parameters_cpu1(1:3)*4 + registration_parameters_cpu2(1:3)*2;
+p(4:12) = registration_parameters_cpu1(4:12) + registration_parameters_cpu2(4:12);
+[x, y, z] = meshgrid(-(sx-1)/2:(sx-1)/2,-(sy-1)/2:(sy-1)/2, -(sz-1)/2:(sz-1)/2);
+x_motion_vectors = zeros(sy,sx,sz);
+y_motion_vectors = zeros(sy,sx,sz);
+z_motion_vectors = zeros(sy,sx,sz);
+x_motion_vectors(:) = p(1) + [x(:) y(:) z(:)]*p(4:6);
+y_motion_vectors(:) = p(2) + [x(:) y(:) z(:)]*p(7:9);
+z_motion_vectors(:) = p(3) + [x(:) y(:) z(:)]*p(10:12);
+
+T1__ = interp3(x,y,z,T1,x+x_motion_vectors,y+y_motion_vectors,z+z_motion_vectors,'linear');    % Generates NaN's
+T1__(isnan(T1__)) = 0;
+ 
+number_of_iterations_for_motion_correction = 10;
+[registered_T1_cpu, registration_parameters_cpu] = perform_T1_MNI_registration_CPU(T1__,MNI,f1,f2,f3,number_of_iterations_for_motion_correction);
+ 
+slice = 128;
+
+figure
+imagesc([ MNI(:,:,slice)/norm(MNI(:)) T1(:,:,slice)/norm(T1(:)) ] ); colormap gray
+figure
+imagesc([ MNI(:,:,slice)/norm(MNI(:)) T1__(:,:,slice)/norm(T1__(:)) ] ); colormap gray
+figure
+imagesc([ MNI(:,:,slice)/norm(MNI(:)) registered_T1_cpu(:,:,slice)/norm(registered_T1_cpu(:)) ] ); colormap gray
+
+
+figure
+imagesc([ MNI(:,:,slice)/norm(MNI(:))  ] ); colormap gray
+figure
+imagesc([ registered_T1_cpu(:,:,slice)/norm(registered_T1_cpu(:)) ]); colormap gray
+skullstripped = registered_T1_cpu .* mask;
+
+for slice = 1:size(skullstripped,3)
+    figure(11)
+    imagesc([ skullstripped(:,:,slice)] ); colormap gray
+    pause(0.1)
+end
+
+
+
+%
+% quadrature_filter_response_reference_1_cpu = convn(fMRI_volumes(:,:,:,1),f1,'same');
+% quadrature_filter_response_reference_2_cpu = convn(fMRI_volumes(:,:,:,1),f2,'same');
+% quadrature_filter_response_reference_3_cpu = convn(fMRI_volumes(:,:,:,1),f3,'same');
+%
+% quadrature_filter_response_aligned_1_cpu = convn(fMRI_volumes(:,:,:,2),f1,'same');
+% quadrature_filter_response_aligned_2_cpu = convn(fMRI_volumes(:,:,:,2),f2,'same');
+% quadrature_filter_response_aligned_3_cpu = convn(fMRI_volumes(:,:,:,2),f3,'same');
+%
+% phase_differences_x_cpu = angle(quadrature_filter_response_reference_1_cpu .* conj(quadrature_filter_response_aligned_1_cpu));
+%
+% phase_certainties_x_cpu = abs(quadrature_filter_response_reference_1_cpu .* quadrature_filter_response_aligned_1_cpu) .* ((cos(phase_differences_x_cpu/2)).^2);
+%
+% phase_gradients_x_cpu = zeros(sy,sx,sz);
+% phase_gradients_x_cpu(:,2:end-1,:) = angle(quadrature_filter_response_reference_1_cpu(:,3:end,:).*conj(quadrature_filter_response_reference_1_cpu(:,2:end-1,:)) + quadrature_filter_response_reference_1_cpu(:,2:end-1,:).*conj(quadrature_filter_response_reference_1_cpu(:,1:end-2,:)) + ...
+%     quadrature_filter_response_aligned_1_cpu(:,3:end,:).*conj(quadrature_filter_response_aligned_1_cpu(:,2:end-1,:)) + quadrature_filter_response_aligned_1_cpu(:,2:end-1,:).*conj(quadrature_filter_response_aligned_1_cpu(:,1:end-2,:)));
+%
+% %
+%
+%
+% slice = 4;
+%
+% figure
+% imagesc( [real(quadrature_filter_response_reference_1_cpu(:,:,slice)) real(quadrature_filter_response_1_opencl(:,:,slice)) ] )
+% figure
+% imagesc( [real(quadrature_filter_response_reference_2_cpu(:,:,slice)) real(quadrature_filter_response_2_opencl(:,:,slice)) ] )
+% figure
+% imagesc( [real(quadrature_filter_response_reference_3_cpu(:,:,slice)) real(quadrature_filter_response_3_opencl(:,:,slice)) ] )
+%
+% figure
+% imagesc( [imag(quadrature_filter_response_reference_1_cpu(:,:,slice)) imag(quadrature_filter_response_1_opencl(:,:,slice)) ] )
+% figure
+% imagesc( [imag(quadrature_filter_response_reference_2_cpu(:,:,slice)) imag(quadrature_filter_response_2_opencl(:,:,slice)) ] )
+% figure
+% imagesc( [imag(quadrature_filter_response_reference_3_cpu(:,:,slice)) imag(quadrature_filter_response_3_opencl(:,:,slice)) ] )
+%
+% figure
+% imagesc( [phase_differences_x_cpu(:,:,slice) phase_differences_x_opencl(:,:,slice) ] )
+% figure
+% imagesc( [phase_certainties_x_cpu(:,:,slice) phase_certainties_x_opencl(:,:,slice) ] )
+% %figure
+% %imagesc( [phase_certainties_x_cpu(:,:,slice) - phase_certainties_x_opencl(:,:,slice) ] ); colorbar
+% figure
+% imagesc( [phase_gradients_x_cpu(:,:,slice) phase_gradients_x_opencl(:,:,slice) ] )
+% figure
+% imagesc( [phase_gradients_x_cpu(2:end-1,2:end-1,slice) - phase_gradients_x_opencl(2:end-1,2:end-1,slice) ] ); colorbar
+%
+%
+%
+%
+% filter_response_1_real_tot_error = sum(abs(real(quadrature_filter_response_aligned_1_cpu(:)) - real(quadrature_filter_response_1_opencl(:))))
+% filter_response_1_real_max_error = max(abs(real(quadrature_filter_response_aligned_1_cpu(:)) - real(quadrature_filter_response_1_opencl(:))))
+% filter_response_1_imag_tot_error = sum(abs(imag(quadrature_filter_response_aligned_1_cpu(:)) - imag(quadrature_filter_response_1_opencl(:))))
+% filter_response_1_imag_max_error = max(abs(imag(quadrature_filter_response_aligned_1_cpu(:)) - imag(quadrature_filter_response_1_opencl(:))))
+%
+% filter_response_2_real_tot_error = sum(abs(real(quadrature_filter_response_aligned_2_cpu(:)) - real(quadrature_filter_response_2_opencl(:))))
+% filter_response_2_real_max_error = max(abs(real(quadrature_filter_response_aligned_2_cpu(:)) - real(quadrature_filter_response_2_opencl(:))))
+% filter_response_2_imag_tot_error = sum(abs(imag(quadrature_filter_response_aligned_2_cpu(:)) - imag(quadrature_filter_response_2_opencl(:))))
+% filter_response_2_imag_max_error = max(abs(imag(quadrature_filter_response_aligned_2_cpu(:)) - imag(quadrature_filter_response_2_opencl(:))))
+%
+% filter_response_3_real_tot_error = sum(abs(real(quadrature_filter_response_aligned_3_cpu(:)) - real(quadrature_filter_response_3_opencl(:))))
+% filter_response_3_real_max_error = max(abs(real(quadrature_filter_response_aligned_3_cpu(:)) - real(quadrature_filter_response_3_opencl(:))))
+% filter_response_3_imag_tot_error = sum(abs(imag(quadrature_filter_response_aligned_3_cpu(:)) - imag(quadrature_filter_response_3_opencl(:))))
+% filter_response_3_imag_max_error = max(abs(imag(quadrature_filter_response_aligned_3_cpu(:)) - imag(quadrature_filter_response_3_opencl(:))))
+%
+% phase_differences_x_tot_error = sum(abs(phase_differences_x_cpu(:) - phase_differences_x_opencl(:)))
+% phase_differences_x_max_error = max(abs(phase_differences_x_cpu(:) - phase_differences_x_opencl(:)))
+%
+% phase_certainties_x_tot_error = sum(abs(phase_certainties_x_cpu(:) - phase_certainties_x_opencl(:)))
+% phase_certainties_x_max_error = max(abs(phase_certainties_x_cpu(:) - phase_certainties_x_opencl(:)))
+%
+% phase_gradients_x_tot_error = sum(sum(sum(abs(phase_gradients_x_cpu(:,2:end-1,:) - phase_gradients_x_opencl(:,2:end-1,:)))))
+% phase_gradients_x_max_error = max(max(max(abs(phase_gradients_x_cpu(:,2:end-1,:) - phase_gradients_x_opencl(:,2:end-1,:)))))
+%
+%
