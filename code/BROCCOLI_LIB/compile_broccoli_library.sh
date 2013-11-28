b14bb0c86ebcdc9f64a8688a0a7fba1548fb0c91
Miha Čančula <miha@noughmad.eu>
2013-11-28 10:03:49 +0100

    Adjust paths in the compile script

--- code/BROCCOLI_LIB/compile_broccoli_library.sh
+++ code/BROCCOLI_LIB/compile_broccoli_library.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BROCCOLI_GIT_DIRECTORY=/home/andek/Research_projects/BROCCOLI/BROCCOLI
-OPENCL_DIRECTORY=/usr/local/cuda-5.0/include/CL #Containing opencl.h
+BROCCOLI_GIT_DIRECTORY=`git rev-parse --show-toplevel`
+OPENCL_DIRECTORY=/usr/include/CL #Containing opencl.h
 
 # Compile BROCCOLI
 nvcc -I${OPENCL_DIRECTORY} -I${BROCCOLI_GIT_DIRECTORY}/code/BROCCOLI_LIB -I${BROCCOLI_GIT_DIRECTORY}/code/BROCCOLI_LIB/Eigen -O3 -m64 -Xcompiler -fPIC -c -o broccoli_lib.o broccoli_lib.cpp
