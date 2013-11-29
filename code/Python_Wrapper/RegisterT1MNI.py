9ceebaac1358e4b41932dace0c6be8fe7b97d4ba
Miha Čančula <miha@noughmad.eu>
2013-11-29 16:53:15 +0100

    Call more methods on the BROCCOLI_LIB object

--- code/Python_Wrapper/RegisterT1MNI.py
+++ code/Python_Wrapper/RegisterT1MNI.py
@@ -1,14 +1,61 @@
 import broccoli
 
+class Array:
+  def __init__(self, data, dimensions, voxel_sizes = None):
+    self.data = data
+    self.dimensions = dimensions
+    if voxel_sizes:
+      self.voxel_sizes = voxel_sizes
+    else:
+      self.voxel_sizes = [1 for i in dimensions]
+    
+class BROCCOLI_EXT(broccoli.BROCCOLI_LIB):
+  def __init__(self, opencl_platform, opencl_device):
+    broccoli.BROCCOLI_LIB.__init__(self, opencl_platform, opencl_device)
+    
+  def SetT1Data(self, array):
+    self.SetT1Width(array.dimensions[0])
+    self.SetT1Height(array.dimensions[1])
+    self.SetT1Depth(array.dimensions[2])
+    self.SetT1VoxelSizeX(array.dimensions[0])
+    self.SetT1VoxelSizeY(array.dimensions[1])
+    self.SetT1VoxelSizeZ(array.dimensions[2])
+    self.SetInputT1Volume(array.data)
+    
+  def SetMNIData(self, array):
+    self.SetMNIWidth(array.dimensions[0])
+    self.SetMNIHeight(array.dimensions[1])
+    self.SetMNIDepth(array.dimensions[2])
+    self.SetMNIVoxelSizeX(array.dimensions[0])
+    self.SetMNIVoxelSizeY(array.dimensions[1])
+    self.SetMNIVoxelSizeZ(array.dimensions[2])
+    self.SetInputMNIVolume(array.data)
+
+def printErrors(BROCCOLI):
+  print("Get platform IDs error is %d" % BROCCOLI.GetOpenCLPlatformIDsError())
+  print("Get device IDs error is %d" % BROCCOLI.GetOpenCLDeviceIDsError())
+  print("Create context error is %d" % BROCCOLI.GetOpenCLCreateContextError())
+  print("Get create context info error is %d" % BROCCOLI.GetOpenCLContextInfoError())
+  print("Create command queue error is %d" % BROCCOLI.GetOpenCLCreateCommandQueueError())
+  print("Create program error is %d" % BROCCOLI.GetOpenCLCreateProgramError())
+  print("Build program error is %d" % BROCCOLI.GetOpenCLBuildProgramError())
+  print("Get program build info error is %d" % BROCCOLI.GetOpenCLProgramBuildInfoError())
+  
+  numOpenKernels = BROCCOLI.GetNumberOfOpenCLKernels()
+  createKernelErrors = BROCCOLI.GetOpenCLCreateKernelErrors()
+  
+  for i in range(numOpenKernels):
+    error = createKernelErrors[i]
+    if error:
+      print("Run kernel error %d is %d" % (i, error))
+
 def registerT1MNI(
-    h_T1_Volume,        # double
-    h_MNI_Volume,       # double
+    h_T1_Data,          # Array
+    h_MNI_Data,         # Array
     h_MNI_Brain,        # double
     h_MNI_Brain_Mask,   # double
-    T1_VOXEL_SIZE,      # 3 elements 
-    MNI_VOXEL_SIZE,     # 3 elements
-    h_Quadrature_Filter_Parametric_Registration,            # 3 elements, complex
-    h_Quadrature_Filter_NonParametric_Registration,         # 6 elements, complex
+    h_Quadrature_Filter_Parametric_Registration,            # 3 elements, complex arrays
+    h_Quadrature_Filter_NonParametric_Registration,         # 6 elements, complex arrays
     h_Projection_Tensor,             # 6 elements
     h_Filter_Directions,             # 3 elements
     NUMBER_OF_ITERATIONS_FOR_PARAMETRIC_IMAGE_REGISTRATION,     # int
@@ -19,31 +66,43 @@ def registerT1MNI(
     OPENCL_DEVICE,          # int
   ):
   
-  b = broccoli.BROCCOLI_LIB(OPENCL_PLATFORM, OPENCL_DEVICE)
-  ok = b.GetOpenCLInitiated()
+  BROCCOLI = BROCCOLI_EXT(OPENCL_PLATFORM, OPENCL_DEVICE)
+  ok = BROCCOLI.GetOpenCLInitiated()
   
   if ok == 0:
-    print("Get platform IDs error is %d" % b.GetOpenCLPlatformIDsError())
-    print("Get device IDs error is %d" % b.GetOpenCLDeviceIDsError())
-    print("Create context error is %d" % b.GetOpenCLCreateContextError())
-    print("Get create context info error is %d" % b.GetOpenCLContextInfoError())
-    print("Create command queue error is %d" % b.GetOpenCLCreateCommandQueueError())
-    print("Create program error is %d" % b.GetOpenCLCreateProgramError())
-    print("Build program error is %d" % b.GetOpenCLBuildProgramError())
-    print("Get program build info error is %d" % b.GetOpenCLProgramBuildInfoError())
-    
-    numOpenKernels = b.GetNumberOfOpenCLKernels()
-    createKernelErrors = b.GetOpenCLCreateKernelErrors()
-    
-    for i in range(numOpenKernels):
-      error = createKernelErrors[i]
-      if error:
-        print("Run kernel error %d is %d" % (i, error))
-
+    printErrors(BROCCOLI)
     print("OpenCL initialization failed, aborting")
-  else:
-    print("OpenCL initialization successful, proceeding...")
+    return
 
+  print("OpenCL initialization successful, proceeding...")
+  
+  BROCCOLI.SetT1Data(h_T1_Data)
+  BROCCOLI.SetMNIData(h_MNI_Data)
+  BROCCOLI.SetInputMNIBrainVolume(h_MNI_Brain)
+  BROCCOLI.SetInputMNIBrainMask(h_MNI_Brain_Mask)
+  
+  BROCCOLI.SetInterpolationMode(1) # Linear
+  BROCCOLI.SetNumberOfIterationsForParametricImageRegistration(NUMBER_OF_ITERATIONS_FOR_PARAMETRIC_IMAGE_REGISTRATION)
+  BROCCOLI.SetNumberOfIterationsForNonParametricImageRegistration(NUMBER_OF_ITERATIONS_FOR_NONPARAMETRIC_IMAGE_REGISTRATION)
+  BROCCOLI.SetImageRegistrationFilterSize(h_Quadrature_Filter_Parametric_Registration[0].dimensions[0])
+  
 if __name__ == "__main__":
-  zeros = [0] * 16
-  registerT1MNI(*zeros)
+  size3 = [1, 1, 1]
+  size1 = [1]
+  data = [1.0]
+  registerT1MNI(
+    Array(data, size3),
+    Array(data, size3),
+    data,
+    data,
+    [Array(data, size3) for i in range(3)],
+    [Array(data, size3) for i in range(6)],
+    [Array(data, size3) for i in range(6)],
+    [Array(data, size3) for i in range(3)],
+    10,
+    10,
+    10,
+    10,
+    0,
+    0
+  )
