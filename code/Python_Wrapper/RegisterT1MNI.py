d0414ae91ddbc04b553daaac2551857517525f5a
Miha Čančula <miha@noughmad.eu>
2013-11-30 10:57:02 +0100

    Convert list to float* on Python side
    This way, we properly support output arguments

--- code/Python_Wrapper/RegisterT1MNI.py
+++ code/Python_Wrapper/RegisterT1MNI.py
@@ -1,5 +1,12 @@
 import broccoli
 
+def floatArrayFromList(lst):
+  n = len(lst)
+  array = broccoli.floatArray(n)
+  for i in range(n):
+    array[i] = lst[i]
+  return array
+
 class Array:
   def __init__(self, data, dimensions, voxel_sizes = None):
     self.data = data
@@ -8,6 +15,9 @@ class Array:
       self.voxel_sizes = voxel_sizes
     else:
       self.voxel_sizes = [1 for i in dimensions]
+      
+  def asFloatArray(self):
+    return floatArrayFromList(self.data)
     
 class BROCCOLI_EXT(broccoli.BROCCOLI_LIB):
   def __init__(self, opencl_platform, opencl_device):
@@ -20,7 +30,7 @@ class BROCCOLI_EXT(broccoli.BROCCOLI_LIB):
     self.SetT1VoxelSizeX(array.dimensions[0])
     self.SetT1VoxelSizeY(array.dimensions[1])
     self.SetT1VoxelSizeZ(array.dimensions[2])
-    self.SetInputT1Volume(array.data)
+    self.SetInputT1Volume(array.asFloatArray())
     
   def SetMNIData(self, array):
     self.SetMNIWidth(array.dimensions[0])
@@ -29,13 +39,13 @@ class BROCCOLI_EXT(broccoli.BROCCOLI_LIB):
     self.SetMNIVoxelSizeX(array.dimensions[0])
     self.SetMNIVoxelSizeY(array.dimensions[1])
     self.SetMNIVoxelSizeZ(array.dimensions[2])
-    self.SetInputMNIVolume(array.data)
+    self.SetInputMNIVolume(array.asFloatArray())
     
   def SetParametricImageRegistrationFilters(self, filters):
     args = []
     for i in range(3):
-      real = [c.real for c in filters[i].data]
-      imag = [c.imag for c in filters[i].data]
+      real = floatArrayFromList([c.real for c in filters[i].data])
+      imag = floatArrayFromList([c.imag for c in filters[i].data])
       args.append(real)
       args.append(imag)
     broccoli.BROCCOLI_LIB.SetParametricImageRegistrationFilters(self, *args)
@@ -43,8 +53,8 @@ class BROCCOLI_EXT(broccoli.BROCCOLI_LIB):
   def SetNonParametricImageRegistrationFilters(self, filters):
     args = []
     for i in range(6):
-      real = [c.real for c in filters[i].data]
-      imag = [c.imag for c in filters[i].data]
+      real = floatArrayFromList([c.real for c in filters[i].data])
+      imag = floatArrayFromList([c.imag for c in filters[i].data])
       args.append(real)
       args.append(imag)
     broccoli.BROCCOLI_LIB.SetNonParametricImageRegistrationFilters(self, *args)
@@ -102,6 +112,9 @@ def registerT1MNI(
 
   print("OpenCL initialization successful, proceeding...")
   
+  ## Pass input parameters to BROCCOLI
+  print("Setting up input parameters")
+  
   BROCCOLI.SetT1Data(h_T1_Data)
   BROCCOLI.SetMNIData(h_MNI_Data)
   BROCCOLI.SetInputMNIBrainVolume(h_MNI_Brain)
@@ -110,26 +123,34 @@ def registerT1MNI(
   BROCCOLI.SetInterpolationMode(broccoli.LINEAR) # Linear
   BROCCOLI.SetNumberOfIterationsForParametricImageRegistration(NUMBER_OF_ITERATIONS_FOR_PARAMETRIC_IMAGE_REGISTRATION)
   BROCCOLI.SetNumberOfIterationsForNonParametricImageRegistration(NUMBER_OF_ITERATIONS_FOR_NONPARAMETRIC_IMAGE_REGISTRATION)
-  BROCCOLI.SetImageRegistrationFilterSize(h_Quadrature_Filter_Parametric_Registration[0].dimensions[0])
   
+  BROCCOLI.SetImageRegistrationFilterSize(h_Quadrature_Filter_Parametric_Registration[0].dimensions[0])
   BROCCOLI.SetParametricImageRegistrationFilters(h_Quadrature_Filter_Parametric_Registration)
   BROCCOLI.SetNonParametricImageRegistrationFilters(h_Quadrature_Filter_NonParametric_Registration)
   
+  BROCCOLI.SetProjectionTensorMatrixFilters(h_Projection_Tensor)
   BROCCOLI.SetFilterDirections(*h_Filter_Directions)
   
+  ## Set up output parameters
+  print("Setting up output parameters")
+  
+  h_Aligned_T1_Volume = broccoli.floatArray(10)
+  BROCCOLI.SetOutputAlignedT1Volume(h_Aligned_T1_Volume)
+
+  
 if __name__ == "__main__":
-  size3 = [1, 1, 1]
-  size1 = [1]
-  data = [1.0]
+  size3 = [3, 1, 1]
+  size1 = [3]
+  data = [1.0, 2.9, 3.7]
   registerT1MNI(
     Array(data, size3),
     Array(data, size3),
-    data,
-    data,
+    floatArrayFromList(data),
+    floatArrayFromList(data),
     [Array(data, size3) for i in range(3)],
     [Array(data, size3) for i in range(6)],
-    [data for i in range(6)],
-    [data for i in range(3)],
+    [[1, 1, 1, 0, 0, 0] for i in range(6)],
+    [floatArrayFromList(data) for j in range(3)],
     10,
     10,
     10,
--- code/Python_Wrapper/broccoli_lib.i
+++ code/Python_Wrapper/broccoli_lib.i
@@ -8,39 +8,9 @@
 
 %include exception.i
 
-%typemap(in) float *
-{
-    /* Check if is a list */
-    if (PyList_Check($input)) {
-        int size = PyList_Size($input);
-        int i = 0;
-        $1 = (float*) malloc((size)*sizeof(float *));
-        for (i = 0; i < size; i++)
-        {
-            PyObject *o = PyList_GetItem($input,i);
-            if (PyFloat_Check(o))
-            {
-                $1[i] = (float)PyFloat_AsDouble(PyList_GetItem($input,i));
-            }
-            else if (PyInt_Check(o))
-            {
-                $1[i] = (float)PyInt_AsLong(PyList_GetItem($input,i));
-            }
-            else
-            {
-                PyErr_SetString(PyExc_TypeError,"list must contain only numbers");
-                free($1);
-                return NULL;
-            }
-        }
-        $1[i] = 0;
-    }
-    else 
-    {
-        PyErr_SetString(PyExc_TypeError,"not a list");
-        return NULL;
-    }
-}
+%include carrays.i
+%array_class(float, floatArray);
+
 
 %typemap(out) int *
 {
