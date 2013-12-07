384719300884a636a069ae23a8b9636e5c5e4536
Miha Čančula <miha@noughmad.eu>
2013-12-07 12:14:36 +0100

    Parse all command-line arguments for T1MNI

--- code/Python_Wrapper/RegisterT1MNI.py
+++ code/Python_Wrapper/RegisterT1MNI.py
@@ -79,6 +79,9 @@ def registerT1MNI(
   h_MNI_Brain = broccoli.packArray(h_MNI_Brain)
   h_MNI_Brain_Mask = broccoli.packArray(h_MNI_Brain_Mask)
   
+  h_MNI_Voxel_Sizes = [round(i) for i in h_MNI_Voxel_Sizes]
+  h_T1_Voxel_Sizes = [round(i) for i in h_T1_Voxel_Sizes]
+  
   ## Pass input parameters to BROCCOLI
   print("Setting up input parameters...")
   
@@ -226,34 +229,31 @@ if __name__ == "__main__":
   
   parser = argparse.ArgumentParser(description='Performs T1 MNI registration')
   
-  parser.add_argument('--opencl_platform', type=int, default=0)
-  parser.add_argument('--opencl_device', type=int, default=0)
+  parser.add_argument('--opencl-platform', type=int, default=0)
+  parser.add_argument('--opencl-device', type=int, default=0)
   
-  parser.add_argument('--mni_file', type=str)
-  parser.add_argument('--mni_brain_file', type=str, default='')
-  parser.add_argument('--mni_brain_mask_file', type=str, default='')
-  parser.add_argument('--t1_file', type=str)
+  parser.add_argument('--mni-file', type=str, required=True)
+  parser.add_argument('--mni-brain-file', type=str)
+  parser.add_argument('--mni-brain-mask-file', type=str)
+  parser.add_argument('--t1-file', type=str, required=True)
   
-  parser.parse_args()
+  parser.add_argument('--iterations-parametric', type=int, default=10)
+  parser.add_argument('--iterations-nonparametric', type=int, default=15)
   
-  opencl_platform = 0
-  opencl_device = 0
+  parser.add_argument('--filters-parametric', type=str, default="../Matlab_Wrapper/filters_for_parametric_registration.mat")
+  parser.add_argument('--filters-nonparametric', type=str, default="../Matlab_Wrapper/filters_for_nonparametric_registration.mat")
   
-  study = 'Cambridge'
-  subject = 'sub00156'
-  voxel_size = 2
+  parser.add_argument('--mm-t1-z-cut', type=int, default=30)
   
-  number_of_iterations_for_parametric_image_registration = 10
-  number_of_iterations_for_nonparametric_image_registration = 15
-  coarsest_scale = 8 / voxel_size
-  MM_T1_Z_CUT = 30
-  #MM_T1_Z_CUT = 120
-
-  (MNI, MNI_brain, MNI_brain_mask, MNI_voxel_sizes) = load_MNI_templates('../../brain_templates/MNI152_T1_%dmm.nii' % voxel_size)
-  (T1, T1_voxel_sizes) = load_T1('../../test_data/fcon1000/classic/%s/%s/anat/mprage_skullstripped.nii.gz' % (study, subject))
+  args = parser.parse_args()
   
-  filters_parametric_mat = scipy.io.loadmat("../Matlab_Wrapper/filters_for_parametric_registration.mat")
-  filters_nonparametric_mat = scipy.io.loadmat("../Matlab_Wrapper/filters_for_nonparametric_registration.mat")
+  (MNI, MNI_brain, MNI_brain_mask, MNI_voxel_sizes) = load_MNI_templates(args.mni_file)
+  (T1, T1_voxel_sizes) = load_T1(args.t1_file)
+  
+  coarsest_scale = int(round(8 / MNI_voxel_sizes[0]))
+  
+  filters_parametric_mat = scipy.io.loadmat(args.filters_parametric)
+  filters_nonparametric_mat = scipy.io.loadmat(args.filters_nonparametric)
   
   parametric_filters = [filters_parametric_mat['f%d_parametric_registration' % (i+1)] for i in range(3)]
   nonparametric_filters = [filters_nonparametric_mat['f%d_nonparametric_registration' % (i+1)] for i in range(6)]
@@ -261,11 +261,11 @@ if __name__ == "__main__":
   results = registerT1MNI(T1, T1_voxel_sizes, MNI, MNI_voxel_sizes, MNI_brain, MNI_brain_mask, parametric_filters, nonparametric_filters,
                 [filters_nonparametric_mat['m%d' % (i+1)][0] for i in range(6)], 
                 [filters_nonparametric_mat['filter_directions_%s' % d][0] for d in ['x', 'y', 'z']],
-                number_of_iterations_for_parametric_image_registration,
-                number_of_iterations_for_nonparametric_image_registration,
+                args.iterations_parametric,
+                args.iterations_nonparametric,
                 coarsest_scale,
-                MM_T1_Z_CUT,
-                opencl_platform,
-                opencl_device)
+                args.mm_t1_z_cut,
+                args.opencl_platform,
+                args.opencl_device)
 
   
