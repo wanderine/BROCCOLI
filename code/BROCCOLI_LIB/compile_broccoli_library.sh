06db01b663f6015099a92f931c86ae8cd6b7a922
Miha Čančula <miha@noughmad.eu>
2013-12-24 18:45:16 +0100

    Default to release mode

--- code/BROCCOLI_LIB/compile_broccoli_library.sh
+++ code/BROCCOLI_LIB/compile_broccoli_library.sh
@@ -8,7 +8,7 @@ OPENCL_DIRECTORY=/usr/include/CL #Containing opencl.h
 DEBUG_FLAGS="-O0 -g"
 RELEASE_FLAGS="-O3 -DNDEBUG"
 
-FLAGS=${DEBUG_FLAGS}
+FLAGS=${RELEASE_FLAGS}
 
 nvcc -I${OPENCL_DIRECTORY} -I${BROCCOLI_GIT_DIRECTORY}/code/BROCCOLI_LIB -I${BROCCOLI_GIT_DIRECTORY}/code/BROCCOLI_LIB/Eigen ${FLAGS} -m64 -Xcompiler -fPIC -c -o broccoli_lib.o broccoli_lib.cpp
 
