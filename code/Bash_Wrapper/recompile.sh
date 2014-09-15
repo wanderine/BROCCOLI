94990b5fe1963eb2c37bf711103e0b6b690e7384
andek <andek034@gmail.com>
2014-09-15 15:21:40 +0200

    Added help scripts for recompiling library and wrappers

---
+++ code/Bash_Wrapper/recompile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../BROCCOLI_LIB
+./compile_broccoli_library.sh
+cd ../Bash_Wrapper
+./compile_wrappers.sh
---
+++ code/Bash_Wrapper/recompile_mac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../BROCCOLI_LIB
+./compile_broccoli_library_mac.sh
+cd ../Bash_Wrapper
+./compile_wrappers_mac.sh
