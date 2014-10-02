9c78eeff4d4a9515487339a5ccf69d532b7ea991
andek <andek034@gmail.com>
2014-10-02 10:46:59 +0200

    Added update script for git

---
+++ code/Bash_Wrapper/update_compiled_bashwrappers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git add ../BROCCOLI_LIB/Compiled/Linux/Release/libBROCCOLI_LIB.a
+
+git add FirstLevelAnalysis
+git add MotionCorrection
+git add RegisterTwoVolumes
+git add RandomiseGroupLevel
+git add SliceTimingCorrection
+git add TransformVolume
+git add GetOpenCLInfo
