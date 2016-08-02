7478d0961c3c252e616eb285799ca61301de6583
Anders Eklund <andek034@gmail.com>
2016-08-02 09:51:26 -0700

    Cleanup randomise test

--- code/testing_scripts/randomise/randomise_scripts.sh
+++
@@ -1,8 +0,0 @@
-time randomise -i betas49.nii.gz -d design1.mat -t design1.con -o permtest -n 9900 -m /home/andek/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz -N -x -P
-
-#export FSLPARALLEL=condor
-#time randomise_parallel -i betas49.nii.gz -d design1.mat -t design1.con -o permtest -n 9900 -m /home/andek/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz -N -x -P
-
-
-
-
